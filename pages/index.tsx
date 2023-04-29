efc53a44d918e01f6f30183775f9caaba73f8d03
seyun(Marco) <ksy90101@gmail.com>
2023-04-29 13:20:36 +0900

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+        <div>
+            <header>
+                헤더
+            </header>
+            <div>
+                내용
+            </div>
+            <footer>
+                본문
+            </footer>
+        </div>
+    )
+}
